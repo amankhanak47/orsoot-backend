334a9b2fe201571c6e5562b519774cedf05ef47c
aman khan <mohdamankhan18@gmail.com>
2022-10-19 22:15:04 +0530

    admin login

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const connectToMongo=require("./database")
 const express = require('express')
-
+// var flash = require('connect-flash');
 var cors = require('cors')
-
+// var AuthController = require('./routes/admin');
 
 connectToMongo();
 
 const app = express();
+// app.use(flash)
 app.use(cors());
 const port = process.env.PORT||5000
 app.get('/', (req, res) => {
@@ -16,7 +17,9 @@ app.get('/', (req, res) => {
 app.use(express.json())
 
 
-app.use('/auth', require('./routes/auth.js'))
+app.use('/user', require('./routes/user.js'))
+app.use('/admin', require('./routes/admin.js'))
+
 
 
 app.listen(port, () => {
